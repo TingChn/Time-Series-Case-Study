5fb938566e1b148686dae13e3bc583c50d0b777c
Valikk202 <suruceanu.valentin@gmail.com>
2024-12-09 12:57:21 +0100

    Update 2.ARIMA.R

--- 2.ARIMA.R
+++ 2.ARIMA.R
@@ -7,40 +7,44 @@ library(dplyr)
 #Not really necessary but is cleaner
 citibike$time <- as.POSIXct(paste(citibike$year, citibike$month, citibike$day, citibike$hour),
                             format="%Y %m %d %H", tz="UTC")
-
-# Filter the data from January 2023 to April 2023
+# Filter the data from January 2023 to April 2023 and hence the rows corresponding to those postions in the data set.
 row_position <- which(rownames(citibike) == "X2023.04.30.23")
 
-# Filter the rows from the beginning to that position
 train_data <- citibike[1:row_position, ]
+test_data <- citibike[(row_position+1):nrow(citibike), ]
 
-# Create a time series object for the 'demand' variable (frequency = 24 hours)
-train_ts <- ts(train_data$demand, start = c(2023, 1), frequency = 24)
+# Create time series objects for the training and testing set (frequency = 24 hours) for the original deman
+train_ts <- ts(train_data$demand, start = c(2023,1), frequency = 24)
+test_ts <- ts(test_data$demand, start = c(2023, 5), frequency = 24)
 
-adf_test <- adf.test(train_ts)
-print(adf_test)
+#Create the data for the series in first differences
+citibike$diff_demand[1] <- 0
+citibike$diff_demand[2:3624] <- diff(citibike$demand)
+
+#Create time series objects for the training and testing
+diff_train_ts <- ts(train_data$diff_demand, start = c(2023,1), frequency = 24)
+diff_test_ts <- ts(test_data$diff_demand, start = c(2023, 5), frequency = 24)
 
 # Plot ACF and PACF to identify the AR and MA terms
 # The lags are scaled because of the hourly data
 #Note that the ACF plot in particular shows seasonality but in PACF we could possibly recognise a pattern
 par(mfrow = c(1, 2))
-acf(train_ts, main = "ACF of Demand")
-pacf(train_ts, main = "PACF of Demand")
+acf(train_ts, main = "ACF of Demand", lag.max = 100)
+pacf(train_ts, main = "PACF of Demand", lag.max = 100)
 
-#Plots for one day
-acf(train_ts, lag.max = 24, main = "ACF of Demand")
-pacf(train_ts, lag.max = 24, main = "PACF of Demand")
+par(mfrow = c(1, 2))
+acf(diff_train_ts, main = "ACF of Change in Demand", lag.max = 100)
+pacf(diff_train_ts, main = "PACF of Change in Demand", lag.max = 100)
+
+par(mfrow = c(1, 2))
+acf(diff_train_ts, main = "ACF of Change in  Demand", lag.max = 12)
+pacf(diff_train_ts, main = "PACF of Change in Demand", lag.max = 12)
 
 #Manual fitting
-#Not sure about this fit still 
-# Fit ARIMA model, assuming AR(1), MA(6) components as a starting point
-fit_arima <- arima(train_ts, order = c(1, 0, 6))
+fit_arima <- arima(train_ts, order = c(2, 1, 1))
 summary(fit_arima)
 
 #Automatic fitting we restrict to non seasonal for now
 fit_arima_auto <- auto.arima(train_ts , seasonal = FALSE)
 summary(fit_arima_auto)
-
-
-#Maybe we should include frequency monthly and yearly?
 
